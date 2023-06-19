e99f57f98b87b55bd3ea6903b34b7f8acb21614a
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-19 15:42:57 +0530

    Counter Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import { useReducer } from "react";
+
+const reducerfn = (currentCounter, action) => {
+
+  switch (action.type) {
+
+    case 'INCREMENT': return currentCounter +1;
+
+    case 'DECREMENT': return currentCounter - 1;
+
+    case 'RESET': return 0;
+    
+    default:  return 0;
+  }
+};
 
 
-function App() {
+const App = () => {
+  const [counter, dispatch] = useReducer(reducerfn,0);
+
+  const onIncrement = () => {
+    dispatch({type: 'INCREMENT'});
+
+  }
+
+  const onDecrement = () => {
+    dispatch({type: 'DECREMENT'});
+  }
+
+  const onreset = () => {
+    dispatch({type: 'RESET'});
+  }
   return (
-    <div className="App">
-      
+    <div>
+    <div>Counter: {counter}</div>
+      <button type="button" onClick={onIncrement}>Increment</button>
+      <button type="button" onClick={onDecrement}>Decrement</button>
+      <button type="button" onClick={onreset}>Reset</button>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
